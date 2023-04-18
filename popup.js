abdf87b9048d81746c89b35ac05d8cc6ae0935e6
smj2173 <smj2173@barnard.edu>
2023-04-18 18:54:07 -0400

    extension improvements

--- popup.js
+++ popup.js
@@ -1,9 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('clickIt');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        alert("This is just a pop up for now, will link to recommendations");
-      });
-    }, false);
-  }, false);
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    var y = document.getElementById("index_link");
+    y.addEventListener("click", openIndex);
+ });
+
+fetch('responses.json')
+   .then((response) => response.json())
+   .then((json) => console.log(json));
+ 
+ function openIndex() {
+   chrome.tabs.create({active: true, url: "https://www.reddit.com/r/cat/comments/12o541i/flooomp/"});
+ }
+
+ $(document).ready(function(){
+   let person = "taylor"
+   $.getJSON('result.json', function(content) {
+      let c = content.taylor
+      console.log(c)
+   });
+});
