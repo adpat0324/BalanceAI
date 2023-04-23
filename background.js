6b4bbfeb45b69744b227deefc8a9b744aa4c6dfd
smj2173 <smj2173@barnard.edu>
2023-04-23 13:19:11 -0400

    more links, better UI

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "testAlarm") {
+        chrome.notifications.create('test', {
+            type: 'basic',
+            iconUrl: 'images/1.png',
+            title: 'Test Message',
+            message: 'You are awesome!',
+            priority: 2
+        });
+    }
+ });
